1b10bf3a80c03245f59b2466efd3689b033c6ebc
arezkidev1 <arezkidev@gmail.com>
2022-10-07 08:47:01 +0200

    testy2

--- tp_sql_papyrus.sql
+++
@@ -1,5 +0,0 @@
--- Active: 1665059872441@@127.0.0.1@3306@papyrus
-
---12. Coder de 2 manières différentes la requête suivante :
---Lister le nom des fournisseurs susceptibles de livrer au moins un article
-SELECT nomfou,qteliv FROM fournis f JOIN ligcom l ON f.numfou=l.numlig;
